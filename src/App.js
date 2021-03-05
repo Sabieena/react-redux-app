f60ddeba3053e51e36d3f07f1e5d30f921b741e7
Sabieena <sabina.bhattrai2015@gmail.com>
2021-03-05 14:39:04 +0545

    action and reducer in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
 import CakeContainer from './components/CakeContainer';
+import store from './redux/store';
 
 function App() {
   return (
+    <Provider store = { store }>
     <div className="App">
       <CakeContainer />
       
     </div>
+    </Provider>
   );
 }
 
